a3181b3dfd8cb0cf0e2e1cf4849a165b21f51fa9
markcty <markcty@outlook.com>
2021-05-30 17:00:49 +0800

    update test

--- main.cpp
+++ main.cpp
@@ -1,46 +1,263 @@
-#include <cstdlib>
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <random>
 #include <vector>
 
 #include "kvstore.h"
+#include "utils.h"
+
 using namespace std;
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+
+KVStore store("../data");
+
+class PutDelayTest {
+ private:
+  int max;
+  random_device rd;
+  mt19937 mt;
+  bool verbose;
+
+ public:
+  explicit PutDelayTest(int max_, bool verbose_)
+      : rd(), mt(rd()), max(max_), verbose(verbose_) {}
+  vector<int> startTest() {
+    cout << ">>>>> Put Delay Test <<<<<" << endl;
+
+    uniform_int_distribution<uint64_t> dist(1, 1024 * 1024 + 1000 * 1024);
+
+    vector<int> time(100);
+    vector<double> avgTime(100);
+    vector<double> throughput(100);
+    vector<int> count(100);
+    vector<int> keys;
+
+    auto totalSize = 0;
+    unsigned long long totalTime = 0;
+    for (int i = 0; i < max; i++) {
+      if (i % 100 == 0) {
+        cout << i << "...";
+        cout.flush();
+      }
+      uint64_t key = dist(mt);
+
+      keys.push_back(key);
+      totalSize += key;
+      string value(key, 's');
+
+      auto start = high_resolution_clock::now();
+      store.put(key, value);
+      auto end = high_resolution_clock::now();
+
+      auto ms = duration_cast<milliseconds>(end - start).count();
+      if (verbose) cout << ms << endl;
+
+      if (key < 256 * 1024) {
+        count[0]++;
+        time[0] += ms;
+      } else if (key < 512 * 1024) {
+        count[1]++;
+        time[1] += ms;
+      } else if (key < 1024 * 1024) {
+        count[2]++;
+        time[2] += ms;
+      } else if (key < 1536 * 1024) {
+        count[3]++;
+        time[3] += ms;
+      } else {
+        count[4]++;
+        time[4] += ms;
+      }
+
+      totalTime += ms;
+    }
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+      avgTime[i] = time[i] / (double)count[i];
+      throughput[i] = 1 / avgTime[i] * 1000;
+    }
+
+    cout << "Average Delay For Different Size Data: " << endl;
+    cout << fixed << setprecision(2);
+    cout << "0 ~ 0.25MB     : " << avgTime[0] << "ms "
+         << "throughput: " << throughput[0] << "/s" << endl;
+    cout << "0.25MB ~ 0.5MB : " << avgTime[1] << "ms "
+         << "throughput: " << throughput[1] << "/s" << endl;
+    cout << "0.5MB ~ 1MB    : " << avgTime[2] << "ms "
+         << "throughput: " << throughput[2] << "/s" << endl;
+    cout << "1MB ~ 1.5MB    : " << avgTime[3] << "ms "
+         << "throughput: " << throughput[3] << "/s" << endl;
+    cout << "1.5MB ~ 2MB    : " << avgTime[4] << "ms "
+         << "throughput: " << throughput[4] << "/s" << endl;
+
+    cout << endl;
+    cout << "Average Delay: " << totalTime / (double)max << "ms, "
+         << "Average Throughput: " << 1 / (totalTime / (double)max) * 1000
+         << "/s" << endl;
+    cout << "Total Size Insert: " << totalSize / 1024 / 1024 << "MB" << endl;
+    cout << endl;
+    return keys;
+  }
+};
+
+class GetDelayTest {
+ public:
+  static void startTest(vector<int> &keys) {
+    random_shuffle(keys.begin(), keys.end());
+
+    vector<int> time(100);
+    vector<double> avgTime(100);
+    vector<double> throughput(100);
+    vector<int> count(100);
+
+    unsigned long long totalTime = 0;
+
+    for (auto key : keys) {
+      auto start = high_resolution_clock::now();
+      store.get(key);
+      auto end = high_resolution_clock::now();
+      auto ms = duration_cast<microseconds>(end - start).count();
+
+      if (key < 256 * 1024) {
+        count[0]++;
+        time[0] += ms;
+      } else if (key < 512 * 1024) {
+        count[1]++;
+        time[1] += ms;
+      } else if (key < 1024 * 1024) {
+        count[2]++;
+        time[2] += ms;
+      } else if (key < 1536 * 1024) {
+        count[3]++;
+        time[3] += ms;
+      } else {
+        count[4]++;
+        time[4] += ms;
+      }
+
+      totalTime += ms;
+    }
+
+    for (int i = 0; i < 5; i++) {
+      avgTime[i] = time[i] / (double)count[i];
+      throughput[i] = 1 / avgTime[i] * 1000 * 1000;
+    }
+
+    cout << ">>>>> Get Delay Test <<<<<" << endl;
+    cout << "Average Delay And Throughput For Different Size Data: " << endl;
+    cout << fixed << setprecision(2);
+    cout << "0 ~ 0.25MB     : " << avgTime[0] << "ms "
+         << "throughput: " << throughput[0] << "/s" << endl;
+    cout << "0.25MB ~ 0.5MB : " << avgTime[1] << "ms "
+         << "throughput: " << throughput[1] << "/s" << endl;
+    cout << "0.5MB ~ 1MB    : " << avgTime[2] << "ms "
+         << "throughput: " << throughput[2] << "/s" << endl;
+    cout << "1MB ~ 1.5MB    : " << avgTime[3] << "ms "
+         << "throughput: " << throughput[3] << "/s" << endl;
+    cout << "1.5MB ~ 2MB    : " << avgTime[4] << "ms "
+         << "throughput: " << throughput[4] << "/s" << endl;
+    cout << endl;
+    cout << "Average Delay: " << totalTime / (double)keys.size() << "µs, "
+         << "Average Throughput: "
+         << 1 / (totalTime / (double)keys.size()) * 1000 * 1000 << "/s" << endl;
+    cout << endl;
+  }
+};
+
+class DeleteDelayTest {
+ public:
+  static void startTest(vector<int> &keys) {
+    random_shuffle(keys.begin(), keys.end());
+
+    vector<int> time(100);
+    vector<double> avgTime(100);
+    vector<double> throughput(100);
+    vector<int> count(100);
+
+    unsigned long long totalTime = 0;
+
+    for (auto key : keys) {
+      auto start = high_resolution_clock::now();
+      store.del(key);
+      auto end = high_resolution_clock::now();
+      auto ms = duration_cast<microseconds>(end - start).count();
+
+      if (key < 256 * 1024) {
+        count[0]++;
+        time[0] += ms;
+      } else if (key < 512 * 1024) {
+        count[1]++;
+        time[1] += ms;
+      } else if (key < 1024 * 1024) {
+        count[2]++;
+        time[2] += ms;
+      } else if (key < 1536 * 1024) {
+        count[3]++;
+        time[3] += ms;
+      } else {
+        count[4]++;
+        time[4] += ms;
+      }
+
+      totalTime += ms;
+    }
+
+    for (int i = 0; i < 5; i++) {
+      avgTime[i] = time[i] / (double)count[i];
+      throughput[i] = 1 / avgTime[i] * 1000 * 1000;
+    }
+
+    cout << ">>>>> Delete Delay Test <<<<<" << endl;
+    cout << "Average Delay And Throughput For Different Size Data: " << endl;
+    cout << fixed << setprecision(2);
+    cout << "0 ~ 0.25MB     : " << avgTime[0] << "ms "
+         << "throughput: " << throughput[0] << "/s" << endl;
+    cout << "0.25MB ~ 0.5MB : " << avgTime[1] << "ms "
+         << "throughput: " << throughput[1] << "/s" << endl;
+    cout << "0.5MB ~ 1MB    : " << avgTime[2] << "ms "
+         << "throughput: " << throughput[2] << "/s" << endl;
+    cout << "1MB ~ 1.5MB    : " << avgTime[3] << "ms "
+         << "throughput: " << throughput[3] << "/s" << endl;
+    cout << "1.5MB ~ 2MB    : " << avgTime[4] << "ms "
+         << "throughput: " << throughput[4] << "/s" << endl;
+    cout << endl;
+    cout << "Average Delay: " << totalTime / (double)keys.size() << "µs, "
+         << "Average Throughput: "
+         << 1 / (totalTime / (double)keys.size()) * 1000 * 1000 << "/s" << endl;
+    cout << endl;
+  }
+};
 
 int main() {
-  srand(time(0));
-  KVStore store("../data");
-  vector<uint64_t> keys;
-  //  for (uint64_t i = 0; i < 5; i++) store.put(i, to_string(i));
-  //  store.del(3);
-  //  store.del(1);
-  //  cout << store.get(1) << endl;
-  //  cout << store.get(2) << endl;
-  //  cout << store.get(3) << endl;
-  //  cout << store.get(4) << endl;
-  //  cout << store.get(0) << endl;
-
-  //  cout << store.get(5) << endl;
-  //  cout << store.get(17736) << endl;
-  //  cout << store.get(572736) << endl;
-  //
-  //  cout << store.get(718282) << endl;
-  //  cout << store.get(371828) << endl;
-  //
-  //  cout << store.get(2371828) << endl;
-  //  cout << store.get(7371828) << endl;
-  //  cout << store.get(5371828) << endl;
-
-  for (uint64_t i = 0; i < 1600000; i++) {
-    auto key = i;
-    keys.push_back(key);
-    store.put(key, to_string(key));
+  string path = "../data";
+  if (utils::dirExists(path)) {
+    string cmd = "rm -rf " + path;
+    system(cmd.c_str());
   }
-  cout << keys[5] << " " << store.get(keys[5]) << endl;
-  cout << keys[9] << " " << store.get(keys[9]) << endl;
-  cout << keys[20] << " " << store.get(keys[20]) << endl;
-  cout << keys[718283] << " " << store.get(keys[718283]) << endl;
-  cout << keys[192874] << " " << store.get(keys[192874]) << endl;
-  cout << keys[1192874] << " " << store.get(keys[1192874]) << endl;
-  cout << keys[892874] << " " << store.get(keys[892874]) << endl;
 
-  return 0;
+  auto start = high_resolution_clock::now();
+  PutDelayTest putDelayTest(1000, false);
+  auto keys = putDelayTest.startTest();
+
+  GetDelayTest::startTest(keys);
+  DeleteDelayTest::startTest(keys);
+
+  if (utils::dirExists(path)) {
+    string cmd = "rm -rf " + path;
+    system(cmd.c_str());
+  }
+
+  auto end = high_resolution_clock::now();
+  auto totalTime = duration_cast<seconds>(end - start).count();
+
+  cout << ">>>>> Summary <<<<<" << endl;
+  cout << "It takes " << totalTime << "s to complete the test." << endl;
 }
